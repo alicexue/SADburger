8ff3d36fd0177348eb95d6ef65af03c6d9082dfa
Derrick Lui <derricklui1@gmail.com>
2016-03-11 11:02:07 -0500

    updated

--- burger.js
+++ burger.js
@@ -13,6 +13,7 @@ var i;
 var fallSpeed = 2;
 var frameNumber;
 
+var restart = false;
 var gameWon;
 
 var level = 1;
@@ -26,86 +27,99 @@ var patties;
 var stack;
 var stackPointer;
 
+var setUpRestart = function() {
+		restart = true;
+		setUp();
+};
+
 // start new game - clear canvas and set up patties
 var setUp = function() {
-    ctx.clearRect(0,0,cWidth,cHeight);
-    frameNumber = 1;
-    gameWon = false;
-	patties = [];
-	stack = [];
-	score = 0;
-	stackPointer = null;
-	patties.push({xcor: Math.floor(Math.random() * (cWidth - 50)) + 25,	ycor: 0});
-	setType(patties[0]);
-	originXcor = cWidth/2;
-	originYcor = cHeight - 40;
+		ctx.clearRect(0,0,cWidth,cHeight);
+		frameNumber = 1;
+		gameWon = false;
+		patties = [];
+		stack = [];
+		
+		if (level == 1) {
+				score = 0;
+		}
+		if (restart){
+				level = 1;
+		}
+		restart = false;
+		stackPointer = null;
+		patties.push({xcor: Math.floor(Math.random() * (cWidth - 50)) + 25,	ycor: 0});
+		setType(patties[0]);
+		originXcor = cWidth/2;
+		originYcor = cHeight - 40;
+
 };
 
 
 var checkWon = function() {
-	if (stackPointer != null && stackPointer["ycor"] - stackPointer["height"] <= 0) {
-		gameWon = true;
-	}
+		if (stackPointer != null && stackPointer["ycor"] - stackPointer["height"] <= 0) {
+				gameWon = true;
+		}
 };
 
 
 var drawOriginBun = function() {
-    ctx.beginPath();
-    ctx.arc(originXcor,originYcor,28,0,Math.PI);
-    ctx.closePath();
-    ctx.strokeStyle = "#FCB54D";
-    ctx.fillStyle = "#FCB54D";
-    ctx.stroke();
-    ctx.fill();
+		ctx.beginPath();
+		ctx.arc(originXcor,originYcor,28,0,Math.PI);
+		ctx.closePath();
+		ctx.strokeStyle = "#FCB54D";
+		ctx.fillStyle = "#FCB54D";
+		ctx.stroke();
+		ctx.fill();
 };
 
 var moveOriginBun = function(e) {
-	var delta;
-	if (e.keyCode == 37 || e.keyCode == 39){
-		if (e.keyCode == 37)
-			delta = -10;
-		if (e.keyCode == 39)
-			delta = 10;
-		var nextStep = originXcor + delta + 28*delta/10;
-		var canMove = true;
-		if (nextStep >= 0 && nextStep <= cWidth) {
-			originXcor += delta;
-		} else {
-			canMove = false;
+		var delta;
+		if (e.keyCode == 37 || e.keyCode == 39){
+				if (e.keyCode == 37)
+						delta = -10;
+				if (e.keyCode == 39)
+						delta = 10;
+				var nextStep = originXcor + delta + 28*delta/10;
+				var canMove = true;
+				if (nextStep >= 0 && nextStep <= cWidth) {
+						originXcor += delta;
+				} else {
+						canMove = false;
+				}
+				updateStack(delta,canMove);
+				drawOriginBun();
 		}
-		updateStack(delta,canMove);
-		drawOriginBun();
-	}
 };
 
 var updateStack = function(delta,canMove){
-	var j = 0;
-	var h;
-	while (j < stack.length){
-		if (canMove)
-			stack[j]["xcor"] += (stack[j]["ycor"] / cHeight + 0.21) * delta;
-		if (Math.abs(stack[j]["xcor"] - originXcor) >= 50){
-			patties.push(stack[j]);
-			if (stack[j] === stackPointer){
-				if (j - 1 < 0)
-					stackPointer = null;
-				else
-					stackPointer = stack[j - 1];
-			}
-			for (h = j + 1; h < stack.length; h++){
-				stack[h]["ycor"] += stack[j]["height"] + 2;
-			}
-			score -= stack[j]["value"];
-			stack.splice(j,1);
-		}else 
-			j++;
-	}
+		var j = 0;
+		var h;
+		while (j < stack.length){
+				if (canMove)
+						stack[j]["xcor"] += (stack[j]["ycor"] / cHeight + 0.21) * delta;
+				if (Math.abs(stack[j]["xcor"] - originXcor) >= 50){
+						patties.push(stack[j]);
+						if (stack[j] === stackPointer){
+								if (j - 1 < 0)
+										stackPointer = null;
+								else
+										stackPointer = stack[j - 1];
+						}
+						for (h = j + 1; h < stack.length; h++){
+								stack[h]["ycor"] += stack[j]["height"] + 2;
+						}
+						score -= stack[j]["value"];
+						stack.splice(j,1);
+				}else 
+						j++;
+		}
 };
 
 
 // Move patties down and check for contact with burger stack
 var updatePatties = function() {
-         if (frameNumber%150 == 0) {
+		if (frameNumber%150 == 0) {
 				// add another patty to the top of the canvas (end of array)
 				patties.push({xcor: Math.floor(Math.random() * (cWidth - 50)) + 25,
 											ycor: 0});
@@ -114,162 +128,143 @@ var updatePatties = function() {
     
 		// move patties down
 		if (frameNumber%2 == 1) {
-			i = 0;
-			while (i<patties.length) {
-				patties[i]["ycor"] += fallSpeed;
-				//attach first patty to originBun
-				if (stackPointer == null &&
-					Math.abs(patties[i]["xcor"] - originXcor) < 50 &&
-					(patties[i]["ycor"] == originYcor || patties[i]["ycor"] == Math.ceil(originYcor/fallSpeed) * fallSpeed)){
-					patties[i]["ycor"] = originYcor-2;
-					stack.push(patties[i]);
-					score += patties[i]["value"];
-					patties.splice(i,1);
-					stackPointer = stack[0];
+				i = 0;
+				while (i<patties.length) {
+						patties[i]["ycor"] += fallSpeed;
+						//attach first patty to originBun
+						if (stackPointer == null &&
+								Math.abs(patties[i]["xcor"] - originXcor) < 50 &&
+								(patties[i]["ycor"] == originYcor || patties[i]["ycor"] == Math.ceil(originYcor/fallSpeed) * fallSpeed)){
+								patties[i]["ycor"] = originYcor-2;
+								stack.push(patties[i]);
+								score += patties[i]["value"];
+								patties.splice(i,1);
+								stackPointer = stack[0];
 				
-				} else if (stackPointer != null &&
-							Math.abs(stackPointer["xcor"]-patties[i]["xcor"]) < 50 &&
-							Math.abs(stackPointer["ycor"]-stackPointer["height"]-patties[i]["ycor"])<=4){
+						} else if (stackPointer != null &&
+											 Math.abs(stackPointer["xcor"]-patties[i]["xcor"]) < 50 &&
+											 Math.abs(stackPointer["ycor"]-stackPointer["height"]-patties[i]["ycor"])<=4){
 								 	
-					patties[i]["ycor"] = stackPointer["ycor"]-stackPointer["height"]-2;
-					stack.push(patties[i]);
-					score += patties[i]["value"];
-					patties.splice(i,1);
-					stackPointer = stack[stack.length - 1];
-					//delete when patty hits bottom
-				} else if (patties[i]["ycor"] >= cHeight){
-					patties.splice(i,1);
-				} else 
-					i++;
+								patties[i]["ycor"] = stackPointer["ycor"]-stackPointer["height"]-2;
+								stack.push(patties[i]);
+								score += patties[i]["value"];
+								patties.splice(i,1);
+								stackPointer = stack[stack.length - 1];
+								//delete when patty hits bottom
+						} else if (patties[i]["ycor"] >= cHeight){
+								patties.splice(i,1);
+						} else 
+								i++;
+				}
 		}
-	}
-	checkWon();
+		checkWon();
 };
 
 //gives patty a random type defined by height and color style
 var setType = function(patty){
-	var pattyType = Math.floor(Math.random() * 100)%6;
-	switch(pattyType){
-	case 1:
-		// wilted lettuce
-		patty["style"] = "#70B226";
-		patty["height"] = 4;
-		patty["value"] = 2;
-		break;
-	case 2:
-		// slice o' tomato
-		patty["style"] = "#DF3232";
-		patty["height"] = 12;    
-		patty["value"] = 3;
-		break;
-	case 3:
-		// good ol' american cheese
-		patty["style"] = "#FFD700";
-		patty["height"] = 8;
-		patty["value"] = 4;
-		break;
-	case 4:
-		// burned mystery meat
-		patty["style"] = "#000000";
-		patty["height"] = 10;
-		patty["value"] = -3;
-		break;
+		var pattyType = Math.floor(Math.random() * 100)%6;
+		switch(pattyType){
+		case 1:
+				// wilted lettuce
+				patty["style"] = "#70B226";
+				patty["height"] = 4;
+				patty["value"] = 2;
+				break;
+		case 2:
+				// slice o' tomato
+				patty["style"] = "#DF3232";
+				patty["height"] = 12;    
+				patty["value"] = 3;
+				break;
+		case 3:
+				// good ol' american cheese
+				patty["style"] = "#FFD700";
+				patty["height"] = 8;
+				patty["value"] = 4;
+				break;
+		case 4:
+				// burned mystery meat
+				patty["style"] = "#000000";
+				patty["height"] = 10;
+				patty["value"] = -3;
+				break;
 	
-	default:
-		// meaty burger patty
-		patty["style"] = "#8B4513";
-		patty["height"] = 20;
-		patty["value"] = 5;
-		break;
-	}
+		default:
+				// meaty burger patty
+				patty["style"] = "#8B4513";
+				patty["height"] = 20;
+				patty["value"] = 5;
+				break;
+		}
 };
 
 // draw different patties depending on patty
 var drawPatty = function(patty){
-	ctx.beginPath();
-	ctx.strokeStyle = patty["style"];
-	ctx.fillStyle = patty["style"];
-	if (patty.hasOwnProperty("height"))
-		ctx.rect(patty["xcor"]-25, patty["ycor"]-patty["height"], 50, patty["height"]);
+		ctx.beginPath();
+		ctx.strokeStyle = patty["style"];
+		ctx.fillStyle = patty["style"];
+		if (patty.hasOwnProperty("height"))
+				ctx.rect(patty["xcor"]-25, patty["ycor"]-patty["height"], 50, patty["height"]);
 	
-	ctx.stroke();
-	ctx.fill();
+		ctx.stroke();
+		ctx.fill();
 };
 
 // draw all patties in patties and stack arrays
 var drawPatties = function() {
-	for (i=0;i<patties.length;i++) {
-			drawPatty(patties[i]);
-	}
-	for (i=0;i<stack.length;i++) {
-			drawPatty(stack[i]);
-	}
+		for (i=0;i<patties.length;i++) {
+				drawPatty(patties[i]);
+		}
+		for (i=0;i<stack.length;i++) {
+				drawPatty(stack[i]);
+		}
 };
 
+// updates score and level on canvas
+var updateText = function() {
+		var levelOffset = 0;
+		ctx.fillStyle="green";
+		ctx.font="30px Helvetica";
+		ctx.fillText("Score",10, 30);
+		ctx.font="20px Helvetica";
+		ctx.fillText(score,10,55);
+
+
+		ctx.fillStyle="blue";
+		ctx.font="30px Helvetica";
+		ctx.fillText("Level",420 + levelOffset,30);
+		ctx.font="20px Helvetica";
+		ctx.fillText(level,480,55);
+
+
+};
 
 var play = function() {
-	ctx.clearRect(0,0,cWidth,cHeight);
-	drawPatties();
-	drawOriginBun();
-	updatePatties();
-	document.getElementById("scorenum").innerHTML = "Score: " + score;
-	document.getElementById("levelnum").innerHTML = "Level " + level;
-	if (gameWon){
-		fallSpeed++;
-		level++;
-		setUp();
-	}		
+		ctx.clearRect(0,0,cWidth,cHeight);
+		updateText();
+		drawPatties();
+		drawOriginBun();
+		updatePatties();
+		document.getElementById("levelnum").innerHTML = "Level " + level;
+		if (gameWon){
+				fallSpeed++;
+				level++;
+				setUp();
+		}		
 		
-	requestID = window.requestAnimationFrame(play);
-	frameNumber++;
+		requestID = window.requestAnimationFrame(play);
+		frameNumber++;
 };
 
 var pause = function() {
-	window.cancelAnimationFrame(requestID);
+		window.cancelAnimationFrame(requestID);
 };
 
 playBtn.addEventListener("click",play);
 pauseBtn.addEventListener("click",pause);
-restartBtn.addEventListener("click",setUp);
+restartBtn.addEventListener("click",setUpRestart);
 window.addEventListener("keydown", moveOriginBun);
 
 setUp();
 
 
-/*
-ctx2.beginPath();
-ctx2.strokeStyle = "#70B226";
-ctx2.fillStyle = "#70B226";
-ctx2.rect(5,5,10, 4);
-ctx2.fill();
-ctx2.stroke();
-ctx2.closePath();
-
-atty["style"] = ;
-		patty["value"] = 2;
-		break;
-	case 2:
-		// slice o' tomato
-		patty["style"] = "#DF3232";
-		patty["height"] = 12;    
-		patty["value"] = 4;
-		break;
-	case 3:
-		// good ol' american cheese
-		patty["style"] = "#FFD700";
-		patty["height"] = 8;
-		patty["value"] = 3;
-		break;
-	case 4:
-		// repulsive mystery meat
-		patty["style"] = "#000000";
-		patty["height"] = 10;
-		patty["value"] = -2;
-		break;
-	
-	default:
-		// meaty burger patty
-		patty["style"] = "#8B4513";
-		patty["height"] = 20;
-		patty["value"] = 5;
-*/
